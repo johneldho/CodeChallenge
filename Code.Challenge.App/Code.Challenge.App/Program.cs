546c35ee8bb76efaf69f8bbf5befcab5dc80cead
Eldho <Eldho.john@illion.com.au>
2019-10-16 20:56:32 +1100

    initial Commit

---
+++ Code.Challenge.App/Code.Challenge.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Code.Challenge.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
